6df13b848dd44d890a4f9bf9a69ee59041c257cc
Mudimba Moonde <mudimbamoonde@gmail.com>
2025-12-11 18:06:39 +0200

    Sperations: OperationParts

---
+++ src/Calculator/OperationParts.kt
@@ -0,0 +1,17 @@
+package Calculator
+
+// Function to add 2 numbers
+fun add(num1 : Double ,num2 : Double  ) : Double {
+    val result =  num1 + num2;
+    return result
+}
+
+fun sub(num1 : Double ,num2 : Double  ) : Double {
+    val result =  num1 -  num2;
+    return result
+}
+
+fun division(num1 : Double ,num2 : Double  ) : Double {
+    val result =  num1 / num2;
+    return result
+}
