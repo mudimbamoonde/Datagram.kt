3a48bc8f9be4636ad9a73445f295aaae0ffd9a59
Mudimba Moonde <mudimbamoonde@gmail.com>
2025-12-11 18:03:45 +0200

    Calculator: do-while loops

--- src/Calculator/dashboard.kt
+++ src/Calculator/dashboard.kt
@@ -1,5 +1,6 @@
 package Calculator
 
+
 /**
  * This is a simple calculator CLI Application
  * its purpose is to do simple Divisions,Addition and Subtraction operations
@@ -10,15 +11,56 @@ fun main(){
     println("\n")
     println("This is a simple Calculator CLI application.")
 
-    println("Enter Option:" +
-            "\n 1. Addition \n" +
-            "2. Subtraction\n" +
-            "3. Division\n" +
-            "4. Exit")
-    val input = readln()
-    val result = input.toInt()
 
-    println(result)
+
+    do {
+        println("Enter Option:\n" +
+                "1. Addition \n" +
+                "2. Subtraction\n" +
+                "3. Division\n" +
+                "4. Exit")
+        val input = readln()
+        val inp = input.toInt()
+
+        if (inp == 1){
+            println("Addition Operations")
+            println("================================")
+            print("Enter Number 1: ")
+            val num1 = readln().toDouble()
+            print("Enter Numbers 2: ")
+            val num2= readln().toDouble()
+            val rst = add(num1,num2)
+            println("The result of $num1 + $num2 = $rst")
+            Thread.sleep(1000L)
+        }else if (inp == 2){
+            println("Subtraction Operations")
+            println("================================")
+            print("Enter Number 1: ")
+            val num1 = readln().toDouble()
+            print("Enter Numbers 2: ")
+            val num2= readln().toDouble()
+            val rst = sub(num1,num2)
+            println("The result of $num1 - $num2 = $rst")
+            Thread.sleep(1000L)
+        }
+        else if (inp == 3){
+            println("Division Operations")
+            println("================================")
+            print("Enter Number 1: ")
+            val num1 = readln().toDouble()
+            print("Enter Numbers 2: ")
+            val num2= readln().toDouble()
+            val rst = division(num1,num2)
+            println("The result of $num1 / $num2 = $rst")
+            Thread.sleep(1000L)
+        }else{
+
+            println("Thank you for using this Calculator!!!")
+            Thread.sleep(100L)
+        }
+
+
+    }while (inp != 4)
 
 
 
@@ -37,6 +79,6 @@ fun sub(num1 : Double ,num2 : Double  ) : Double {
 }
 
 fun division(num1 : Double ,num2 : Double  ) : Double {
-    val result =  num1 % num2;
+    val result =  num1 / num2;
     return result
 }
